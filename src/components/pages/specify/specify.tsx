3116d8e61a84566caed4901e4f8ecedcd2c3b87f
Nacho077 <ignaciogimenez70@gmail.com>
2021-01-10 15:10:02 -0300

    table

--- src/components/pages/specify/specify.tsx
+++ src/components/pages/specify/specify.tsx
@@ -8,15 +8,33 @@ type SpecifyProps = {
 }
 
 const Specify: React.FC<SpecifyProps> = ({selected}): JSX.Element => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch() 
     const info = useSelector((state: RootStore) => state.type.info)
+    const types = useSelector((state: RootStore) => state.type.types)
     useEffect(() => {
         dispatch(getSpecify(selected))
     }, [dispatch, selected])
     console.log(info)
 
     return(
-        <h1>{selected.replace(/\b\w/g, a => a.toUpperCase())}</h1>
+        <>
+            <h1>{selected.replace(/\b\w/g, a => a.toUpperCase())}</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        {info ? Object.keys(info.damage_relations).map(relation => relation.replace(/\b\w/g, a => a.toUpperCase()).split("_").join(" ")).map(relation => (
+                            <th>{relation}</th>
+                        )) : null}
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </>
     )
 }
 
